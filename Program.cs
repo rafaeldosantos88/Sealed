1529f99a37ddf483c85102231b0c1abd87436406
Rafael Dos Santos <raphael_979@hotmail.com>
2019-10-11 14:01:24 -0300

    Pasta criada (entities)

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Metodosselado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
